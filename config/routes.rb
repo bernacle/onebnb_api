9ab67114350a02edd43319cc08cd428916662e29
Bruno Simplicio <brunoraphael89@gmail.com>
2017-01-24 21:26:43 -0200

    Configure Cloudnary and upload a image

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:update]
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        passwords:          'devise_token_auth/passwords'
+      }
       resources :properties
     end
   end
