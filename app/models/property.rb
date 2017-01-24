d014b28f8b4f2614af73f922faf40b229e0fefd8
Bruno Simplicio <brunoraphael89@gmail.com>
2017-01-24 21:45:29 -0200

    Create the remaining tables of the app

---
+++ app/models/property.rb
@@ -0,0 +1,15 @@
+class Property < ApplicationRecord
+  # Os possiveis status de uma Propriedade
+  enum status: [:active, :pending, :inactive, :blocked]
+
+  # Os tipos de acomodação: casa inteira, quarto inteiro e quarto compartilhado
+  enum accommodation_type: [:whole_house, :whole_bedroom, :shared_bedroom]
+
+
+  belongs_to :user
+  belongs_to :address
+  belongs_to :facility
+  has_many :wishlists
+  has_many :photos
+
+end
